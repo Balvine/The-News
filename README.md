b65fcd8aac5df5a8086ca2670d469b5c7419103e
Balvine <balvy075@gmail.com>
2020-09-17 11:12:22 +0300

    added readme markdwon file

---
+++ README.md
@@ -0,0 +1,39 @@
+# NEWS HIGHLIGHTS
+
+This ia an application that allows a users to views popular news, upcoming news and now showing news.They can also be able to read more information in any news.
+
+## Author
+
+BALVINE MBARI
+
+## Behaviour Driven Development(BDD)
+
+### Input
+
+click on news title
+
+#### Output
+
+News detail display
+
+## Installation
+
+Step to view this application :
+1 Clone this repo. ()>
+
+2 Open the repo using your preffered text editor e.g vscode.
+
+3 Run the test file to confirm that all the test passes.
+
+4 Run the main file to view how the application runs.
+
+## Technologies Used
+
+1 Python
+2 Flask
+3 Pip to install flask
+
+## Contact details
+
+
+## License
--- manage.py
+++ manage.py
@@ -1,18 +1,30 @@
+# from app import create_app
+# from flask_script import Manager,Server
+
+# # Creating app instance
+# app = create_app('development')
+
+# manager = Manager(app)
+# manager.add_command('server',Server)
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('test')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+# if __name__ =='__main__':
+#     manager.run()
 from app import create_app
-from flask_script import Manager,Server
+from flask_script import Manager, Server
 
 # Creating app instance
-app = create_app('development')
+app = create_app("development")
 
 manager = Manager(app)
-manager.add_command('server',Server)
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('test')
-    unittest.TextTestRunner(verbosity=2).run(tests)
 
+manager.add_command("server", Server)
 
-if __name__ =='__main__':
+if __name__ == "__main__":
     manager.run()
